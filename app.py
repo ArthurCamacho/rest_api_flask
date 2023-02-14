fb67ef84fc1f04341bce7398eecd177bfeb95a82
ArthurCamacho <arthur.marques0308@outlook.com>
2023-02-14 09:04:47 -0300

    Implementando individuos da coleção Seguros. Implementado GET e POST no endpoint /seguros/{seguro_id}

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource
-from resources.seguro import Seguros
+from resources.seguro import Seguros, Seguro
 
 
 # Instancia o aplicativo
@@ -11,6 +11,7 @@ api = Api(app)
 
 # Disponibilizandoa classe como coleção e criando seu endpoint
 api.add_resource(Seguros, '/seguros')
+api.add_resource(Seguro, '/seguros/<int:seguro_id>')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- resources/seguro.py
+++ resources/seguro.py
@@ -1,39 +1,80 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 
 # Dentro do pacote de recursos iremos colocar todas as classe e todo o processamento da API
 # para poder importa no app.py e adicionar o endpoint na API
 
 # Criando a lista de dicionarios com os dados dos seguros para exemplificar
-lista_seguros = [
+seguros = [
     {
-        "ID": 12,
-        "Tipo": "Auto",
-        "Descricao": "O seguro destinado para carros",
-        "Valor": 480.20
+        "id": 12,
+        "tipo": "Auto",
+        "descricao": "O seguro destinado para carros",
+        "valor": 480.20
     },
     {
-        "ID" : 13,
-        "Tipo" : "Auto",
-        "Descricao" : "O seguro destinado para Caminhonetes",
-        "Valor" : 780.56
+        "id" : 13,
+        "tipo" : "Auto",
+        "descricao" : "O seguro destinado para Caminhonetes",
+        "valor" : 780.56
     },
     {
-        "ID" : 15,
-        "Tipo" : "Vida",
-        "Descricao" : "O seguro destinado para tranquilizar você e a sua família nos piores momentos",
-        "Valor" : 1200.85
+        "id" : 15,
+        "tipo" : "Vida",
+        "descricao" : "O seguro destinado para tranquilizar você e a sua família nos piores momentos",
+        "valor" : 1200.85
     },
     {
-        "ID" : 25,
-        "Tipo" : "Residencial",
-        "Descricao" : "O seguro destinado para proteger sua casa e seu bolso",
-        "Valor" : 450.00
+        "id" : 25,
+        "tipo" : "Residencial",
+        "descricao" : "O seguro destinado para proteger sua casa e seu bolso",
+        "valor" : 450.00
     },
 ]
 
 #Criando uma classe como uma coleção
 class Seguros(Resource):
-    # Definindo metodo get
+    # Definindo a coleção Seguros
     def get(self):
-        return {'Seguros' : lista_seguros}
+        return {'seguros' : seguros}
+
+class Seguro(Resource):
+    #Definindo os individuos da coleção Seguros
+    def get(self, seguro_id):
+
+        for seguro in seguros:
+            if seguro['id'] == seguro_id:
+                return seguro, 200
+        return {"message" : "Não há seguro(s) com esse identificador para serem encontrados"}, 404
+    def post(self, seguro_id):
+        # Necessita instanciar um objeto com a classe do parser para poder ler todos os argumentos passados na requisiçãp
+        # é necessário fazer um self.addArgument("<nome_argumento>") para cada chave q se deseja ler o valor no corpo
+        # da requisição. O id vem na url (Mas isso n deveria ser assim, tinha que ser auto_increment)
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('tipo')
+        argumentos.add_argument('descricao')
+        argumentos.add_argument('valor')
+
+        try:
+            # Com o parse args nós podemos ler todos as chaves dos add_arguments acima e salvar tudo em  um dicionario
+            dados_requisicao = argumentos.parse_args()
+
+            novo_seguro = {
+                "id" : seguro_id,
+                "tipo" : dados_requisicao['tipo'],
+                "descricao" : dados_requisicao['descricao'],
+                "valor" : dados_requisicao['valor']
+            }
+
+            seguros.append(novo_seguro)
+
+        except:
+            return {'message' : 'Falha ao inserir o novo seguro. Verifique se todos os dados necessários foram preenchidos e se os dados informados foram preenchidos corretamente'}, 406
+
+        return {'message' : 'Novo seguro criado com sucesso'}, 200
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
